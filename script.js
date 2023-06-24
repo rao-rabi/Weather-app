fabbb5f1266d889a4347143e5e56f2f9af41d7bc
rao rabi <rabirao1039@gmail.com>
2023-06-24 20:41:01 +0500

    new changes done

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-let cityname = document.getElementById('city');
-let temperature = document.getElementById('temp');
-let wind = document.getElementById('wind');
-let desc = document.getElementById('desc');
-let city = prompt("Enter City Name.")
-let weatherApi = fetch(`https://goweather.herokuapp.com/weather/${city}`);
-weatherApi.then((response) => {
-    console.log(response.status);
-    console.log(response.ok);
-    return response.json();
-}).then((value) => {
-    console.log(value);
-    cityname.innerText = `City: ${city}`;
-    temperature.innerText = `Temprature: ${value.temperature}`;
-    wind.innerText =`Wind: ${value.wind}`;
-    desc.innerText =`Weather: ${value.description}`;
-})
+function clickMe() {
+    let input = document.getElementById('input');
+    let cityname = document.getElementById('city');
+    let temperature = document.getElementById('temp');
+    let wind = document.getElementById('wind');
+    let desc = document.getElementById('desc');
+
+    let weatherApi = fetch(`https://goweather.herokuapp.com/weather/${input.value}`);
+    weatherApi.then((response) => {
+        console.log(response.status);
+        console.log(response.ok);
+        return response.json();
+    }).then((value) => {
+        console.log(value);
+        cityname.innerText = `${input.value.toUpperCase()}`;
+        temperature.innerText = `Temprature: ${value.temperature}`;
+        wind.innerHTML = `<img src="/images/wind.png" width="50px" height="50px">
+                        <div style="margin-top:-22px;"><p style="margin-bottom:0px;font-size:38px">${value.wind}</p>
+                        <p style="font-size:15px;margin-top:0px;">Wind Speed</p></div>`;
+        desc.innerText = `Weather: ${value.description}`;
+    })
+}
